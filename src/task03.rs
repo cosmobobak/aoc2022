3c0bac3196fb4e25a408077835b09ba7531c3c2d
Cosmo Bobak <cosmobobak@gmail.com>
2022-12-03 23:10:45 +0000

    bitmask sol

--- src/task03.rs
+++ src/task03.rs
@@ -6,7 +6,7 @@ use itertools::Itertools;
 
 const OFFSET: u8 = 32;
 
-fn map_letter(l: u8) -> u8 {
+fn priority(l: u8) -> u8 {
     match l {
         b'a'..=b'z' => l - b'a',
         b'A'..=b'Z' => l - b'A' + 26,
@@ -14,25 +14,31 @@ fn map_letter(l: u8) -> u8 {
     }
 }
 
-fn mask(s: &str) -> u128 { s.bytes().fold(0, |acc, l| acc | (1 << (l - OFFSET))) }
+fn mask(s: &str) -> u128 {
+    s.bytes().fold(0, |acc, l| acc | (1 << (l - OFFSET)))
+}
 
 pub fn task03() {
     let input = include_str!("../tasks/task03.txt");
 
-    let total_priority = input.lines().map(|line| line.split_at(line.len() / 2))
+    let total_priority = input
+        .lines()
+        .map(|line| line.split_at(line.len() / 2))
         .map(|(a, b)| {
             let shared_char = mask(a) & mask(b);
-            u64::from(map_letter(shared_char.trailing_zeros() as u8 + OFFSET)) + 1
+            u64::from(priority(shared_char.trailing_zeros() as u8 + OFFSET)) + 1
         })
         .sum::<u64>();
 
     println!("Part 1: {total_priority}");
 
-    let total_priority = input.lines().chunks(3)
+    let total_priority = input
+        .lines()
+        .chunks(3)
         .into_iter()
         .map(|c| {
             let shared_char = c.fold(!0, |acc, line| acc & mask(line));
-            u64::from(map_letter(shared_char.trailing_zeros() as u8 + OFFSET)) + 1
+            u64::from(priority(shared_char.trailing_zeros() as u8 + OFFSET)) + 1
         })
         .sum::<u64>();
 
