89008766374c884c7335ac0322d9a687d6298452
Cosmo Bobak <cosmobobak@gmail.com>
2022-12-05 20:45:23 +0000

    neater

--- src/task05.rs
+++ src/task05.rs
@@ -1,78 +1,122 @@
 
+#[derive(Clone, Copy)]
+struct StaticStack<T: Copy, const CAPACITY: usize> {
+    stack: [T; CAPACITY],
+    top: usize,
+}
+
+impl<const CAPACITY: usize> StaticStack<u8, CAPACITY> {
+    const fn new() -> Self {
+        Self {
+            stack: [0; CAPACITY],
+            top: 0,
+        }
+    }
+
+    fn push(&mut self, value: u8) {
+        if self.top < CAPACITY {
+            self.stack[self.top] = value;
+            self.top += 1;
+        }
+    }
+
+    fn pop(&mut self) -> Option<u8> {
+        if self.top > 0 {
+            self.top -= 1;
+            Some(self.stack[self.top])
+        } else {
+            None
+        }
+    }
+
+    fn data_mut(&mut self) -> &mut [u8] {
+        &mut self.stack[..self.top]
+    }
+
+    const fn peek(&self) -> Option<u8> {
+        if self.top > 0 {
+            Some(self.stack[self.top - 1])
+        } else {
+            None
+        }
+    }
+
+    const fn len(&self) -> usize {
+        self.top
+    }
+
+    fn add_slice(&mut self, slice: &[u8]) {
+        for &value in slice {
+            self.push(value);
+        }
+    }
+
+    fn drop_last(&mut self, n: usize) {
+        self.top = self.top.saturating_sub(n);
+    }
+}
+
+fn borrow_indexes<T>(slice: &mut [T], i1: usize, i2: usize) -> (&mut T, &mut T) {
+    if i1 < i2 {
+        let split = slice.split_at_mut(i2);
+        (&mut split.0[i1], &mut split.1[0])
+    } else {
+        let split = slice.split_at_mut(i1);
+        (&mut split.1[0], &mut split.0[i2])
+    }
+}
 
 pub fn task05() {
     let (arrangement, instructions) = include_str!("../tasks/task05.txt").split_once("\r\n\r\n").unwrap();
 
-    let mut heights = [0usize; 9];
-    let mut stacks = [[0; 100]; 9];
-    for (i, line) in arrangement.lines().rev().skip(1).enumerate() {
+    let mut stacks = [StaticStack::<u8, 100>::new(); 9];
+    for line in arrangement.lines().rev().skip(1) {
         let bytes = line.as_bytes();
-        for (j, h) in heights.iter_mut().enumerate() {
+        for (j, stack) in stacks.iter_mut().enumerate() {
             let value = bytes[j * 4 + 1];
-            stacks[j][i] = value;
             if value != b' ' {
-                *h = i + 1;
+                stack.push(value);
             }
         }
     }
 
-    let heights_part_2 = heights;
-    let stacks_part_2 = stacks;
-    
-    let mut make_move = |from: usize, to: usize| {
-        let fh = heights[from];
-        let th = heights[to];
-        let value = std::mem::replace(&mut stacks[from][fh - 1], b' ');
-        stacks[to][th] = value;
-        heights[from] -= 1;
-        heights[to] += 1;
-    };
-
-    for instruction in instructions.lines() {
-        let mut parts = instruction.split_whitespace().filter_map(|p| p.parse::<usize>().ok());
+    let parse_inst = |inst: &str| {
+        let mut parts = inst.split_whitespace().filter_map(|p| p.parse::<usize>().ok());
         let n = parts.next().unwrap();
         let from = parts.next().unwrap() - 1;
         let to = parts.next().unwrap() - 1;
+        (from, to, n)
+    };
+
+    let stacks_part_2 = stacks;
+
+    for instruction in instructions.lines() {
+        let (from, to, n) = parse_inst(instruction);
         for _ in 0..n {
-            make_move(from, to);
+            let val = stacks[from].pop().unwrap();
+            stacks[to].push(val);
         }
     }
 
     print!("Part 1: ");
-    for (i, &h) in heights.iter().enumerate() {
-        print!("{}", stacks[i][h - 1] as char);
+    for s in &stacks {
+        print!("{}", s.peek().unwrap() as char);
     }
     println!();
 
-    let mut heights = heights_part_2;
     let mut stacks = stacks_part_2;
-    let mut make_move_9001 = |from: usize, to: usize, n: usize| {
-        let (from_stack, to_stack) = if from < to {
-            let split = stacks.split_at_mut(to);
-            (&mut split.0[from], &mut split.1[0]) 
-        } else {
-            let split = stacks.split_at_mut(from);
-            (&mut split.1[0], &mut split.0[to]) 
-        };
-        let slice_to_read = &mut from_stack[heights[from] - n..heights[from]];
-        let slice_to_write = &mut to_stack[heights[to]..heights[to] + n];
-        slice_to_write.copy_from_slice(slice_to_read);
-        slice_to_read.fill(b' ');
-        heights[from] -= n;
-        heights[to] += n;
-    };
-
     for instruction in instructions.lines() {
-        let mut parts = instruction.split_whitespace().filter_map(|p| p.parse::<usize>().ok());
-        let n = parts.next().unwrap();
-        let from = parts.next().unwrap() - 1;
-        let to = parts.next().unwrap() - 1;
-        make_move_9001(from, to, n);
+        let (from, to, n) = parse_inst(instruction);
+        let (from_stack, to_stack) = borrow_indexes(&mut stacks, from, to);
+        let from_len = from_stack.len();
+        let slice_to_read = &mut from_stack.data_mut()[from_len - n..];
+        to_stack.add_slice(slice_to_read);
+        from_stack.drop_last(n);
     }
 
     print!("Part 2: ");
-    for (i, &h) in heights.iter().enumerate() {
-        print!("{}", stacks[i][h - 1] as char);
+    for s in &stacks {
+        print!("{}", s.peek().unwrap() as char);
     }
     println!();
 }
