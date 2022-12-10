b7849e337b43f982f809d58dd66d9a134656fe61
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2022-12-10 18:40:30 +0000

    remove allocation

--- src/task10.rs
+++ src/task10.rs
@@ -1,4 +1,4 @@
-
+#![allow(clippy::cast_sign_loss)]
 
 pub fn task10() {
     let start = std::time::Instant::now();
@@ -7,12 +7,11 @@ pub fn task10() {
     let mut x = 1;
     let mut clocks = 1;
     let mut cycle_sum = 0;
-    let mut display = String::new();
+    let mut display = [0; 6 * 40];
     let is_drawn = |register: i32, clock: i32| ((clock - 1) % 40).abs_diff(register) <= 1;
     let mut cycle = |r| {
         if clocks - 20 >= 0 && (clocks - 20) % 40 == 0 { cycle_sum += r * clocks; }
-        display.push([' ', '@'][usize::from(is_drawn(r, clocks))]);
-        if clocks % 40 == 0 { display.push_str("\r\n"); }
+        display[clocks as usize - 1] = [b' ', b'@'][usize::from(is_drawn(r, clocks))];
         clocks += 1;
     };
 
@@ -29,7 +28,10 @@ pub fn task10() {
     }
 
     println!("Part 1: {cycle_sum}");
-    println!("Part 2: \n{display}");
+    println!("Part 2:");
+    for row in display.chunks(40) {
+        println!("{}", std::str::from_utf8(row).unwrap());
+    }
 
     let elapsed = start.elapsed();
     println!("Elapsed: {:.3}ms", elapsed.as_secs_f64() * 1000.0);
